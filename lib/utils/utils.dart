4bbe09d2672a2b9be6591f4493f36aaebcdda681
arnold <arnold.wabo@gmail.com>
2023-08-16 11:14:57 +0100

    - retrait des method channels - maj des dependances - exposition des librairies internes

---
+++ lib/utils/utils.dart
@@ -0,0 +1,5 @@
+library potatoes;
+export 'package:dio/src/response.dart';
+export 'package:equatable/equatable.dart';
+export 'package:package_info_plus/package_info_plus.dart';
+export 'package:shared_preferences/shared_preferences.dart';
