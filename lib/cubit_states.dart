2cfea5691a16648a61d2a09b963c248dc4405dd5
arnold <arnold.wabo@gmail.com>
2023-12-06 13:33:14 +0100

    - intégration du value_cubit.dart - ajout du cubit manager à l'export des cubit states

--- lib/cubit_states.dart
+++ lib/cubit_states.dart
@@ -1,3 +1,5 @@
 export 'package:equatable/equatable.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:potatoes/bloc/cubit_manager.dart';
 export 'package:potatoes/bloc/cubit_state.dart';
+export 'package:potatoes/bloc/value_cubit.dart';
